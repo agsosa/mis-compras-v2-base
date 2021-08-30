598941de7b4ff7f1bde7c2cb1ade833b71654338
Alejandro Sosa <aleej.gs@gmail.com>
2021-08-30 13:02:55 -0300

    Add React hook test example

--- src/components/TestFooter.tsx
+++ src/components/TestFooter.tsx
@@ -1,12 +1,23 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Container } from "./TestFooter.styled";
+import useCounter from "@hooks/useCounter";
 
 interface TestFooterProps {
   testProp: string;
 }
 
 const TestFooter: FC<TestFooterProps> = ({ testProp }) => {
-  return <Container>{testProp}</Container>;
+  const { count, increment } = useCounter();
+
+  useEffect(() => {
+    setTimeout(() => increment(), 3000);
+  }, [increment]);
+
+  return (
+    <Container>
+      {testProp} {count}
+    </Container>
+  );
 };
 
 export default TestFooter;
